0c6ce534d80e1f104a62bf16aef3c57e3047c784
AudreyPic <audrey.picaud@etu.unilim.fr>
2023-10-17 19:46:24 +0200

    Update UserCrud

--- src/Controller/Admin/UsersCrudController.php
+++ src/Controller/Admin/UsersCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Users;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UsersCrudController extends AbstractCrudController
 {
@@ -12,14 +13,13 @@ class UsersCrudController extends AbstractCrudController
         return Users::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('email'),
+            TextField::new('password')
         ];
     }
-    */
+
 }
